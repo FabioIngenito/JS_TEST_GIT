41c1814210bd289cacfe0d0778cef3f3f4409ddc
FabioIngenito <fabio.ingenito@hotmail.com>
2023-01-26 14:29:08 -0300

    exemplo da aula 1

---
+++ type-string.js
@@ -0,0 +1,26 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaseupersegura123456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'O leo disse: "Oi!"';
+const citacao2 = " - O leo disse: 'Oi!'";
+console.log(citacao);
+console.log(citacao2);
+
+//template string ou template literal
+
+const citacao3 = `The sum of ${citacao} and ${citacao2}`;
+console.log(citacao3);
+
+console.log(citacao + citacao2);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
