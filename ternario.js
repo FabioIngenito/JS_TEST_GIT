6cf03d3cc46519538239b461e982e4179458ab1e
FabioIngenito <fabio.ingenito@hotmail.com>
2023-01-29 14:50:28 -0300

    Operadores

---
+++ ternario.js
@@ -0,0 +1,12 @@
+const idadeMinima = 18;
+const idadeCliente = 17;
+
+if (idadeCliente >= idadeMinima) {
+  console.log("Cerveja");
+} else {
+  console.log("Suco");
+}
+//Operador Ternário (<=, ?, :)
+//Condição ? TRUE :  FALSE
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco");
+
