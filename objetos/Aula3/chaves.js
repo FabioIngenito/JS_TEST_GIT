39c09f656817d11e094026b33d5f75ec955344c2
FabioIngenito <fabio.ingenito@hotmail.com>
2023-02-10 09:04:37 -0300

    Percorrendo objetos

---
+++ objetos/Aula3/chaves.js
@@ -0,0 +1,26 @@
+const cliente = {
+  nome: "Joao",
+  idade: 24,
+  email: "joao@firma.com",
+  telefone: ["1155555550", "1144444440"],
+};
+
+cliente.enderecos = [
+  {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+  },
+];
+
+const chavesDoObjeto = Object.keys(cliente);
+
+console.log(chavesDoObjeto);
+
+if (!chavesDoObjeto.includes("enderecos")) {
+  console.error("Erro. É necessário ter um endereço cadastrado.")
+}
+
+// Trabalhando com objetos
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Working_with_Objects
